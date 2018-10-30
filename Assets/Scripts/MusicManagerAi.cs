2fe34d1f7fd3d4b23a6a7bd5b76a336f4576e6b4
petercarmona <carmonismo@hotmail.com>
2018-10-30 16:59:39 -0500

    yay

---
+++ Assets/Scripts/MusicManagerAi.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManagerAi : MonoBehaviour {
+
+    public GameObject[] musicHolder;
+    public Track[] repertory = new Track[5];
+    public Material[] mat = new Material[3];
+
+    public int holderSize = 3;
+    public float offSet = 3.5f;
+    public int musicSelection = 0;
+
+    public Dictionary <int,Track> trackList;
+    // Use this for initialization
+    void Start() {
+        musicHolder = new GameObject[holderSize];
+        trackList = new Dictionary<int, Track>();
+     
+        for (int i = 0; i < holderSize; i++)
+        {
+            musicHolder[i] = Resources.Load("MusicHolders") as GameObject;
+            trackList.Add(i, );
+        }
+
+        InstantiateTrackList();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        movement();
+	}
+
+    void movement()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            Vector3 newPos = transform.position;
+            newPos.x += offSet;
+            transform.position = newPos;
+            musicSelection--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 newPos = transform.position;
+            newPos.x -= offSet;
+            transform.position = newPos;
+            musicSelection++;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        { 
+            Transform selectable = transform.Find("Music " + musicSelection);
+            Vector3 tmp = selectable.position;
+            tmp.y += 2.5f;
+            selectable.position = tmp;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+
+        }
+
+    }
+
+    void InstantiateTrackList()
+    {
+        Vector3 tmp = transform.position;
+        for (int i = 0; i < holderSize; i++)
+        {
+            tmp.x = i * 3.5f;
+
+            GameObject go = Instantiate(musicHolder[i], tmp, Quaternion.identity) as GameObject;
+            go.transform.SetParent(transform);
+            go.name = "Music " + (i);
+            go.GetComponent<Renderer>().material = mat[Random.Range(0, 3)];
+        }
+    }
+}
+    
---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Track {
+
+    private string _trackName;
+    private int _song; // Int being placeholder
+    private int _dificulty;
+    private string _author;
+    
+    public string trackName { get { return _trackName; } set { value = _trackName; } }
+    public int song { get { return _song; } set { value = _song; } }
+    public int difculty { get { return _dificulty; } set { value = _dificulty; } }
+    public string author { get { return _author; } set { value = _author; } }
+
+    Track()
+    {
+        _trackName = "undefined";
+        _song = -1;
+        _dificulty = -1;
+        _author = "undefined";
+    }
+
+    Track(string trackName, int song, int dificulty, string author)
+    {
+        _trackName = trackName;
+        _song = song;
+        _dificulty = dificulty;
+        _author = author;
+    }
+}
